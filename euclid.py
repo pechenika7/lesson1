8e17661b5b0a07aa7fe6e545ec85bbbbe846cbe5
unknown <viktoriamyazina7@gmail.com>
2023-11-16 11:40:02 +0300

    euclid by division classic

--- euclid.py
+++ euclid.py
@@ -6,6 +6,15 @@ def Eucl1(a,b):
             b = b - a
     return(a)
 
+def Eucl2(a,b):
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    return(a+b)
+
 a = int(input())
 b = int(input())
 print(Eucl1(a,b))
+print(Eucl2(a,b))
